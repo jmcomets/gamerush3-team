4667fb5b5fd59d4a94c33432895c842e336542e5
eoger <edouard.oger@gmail.com>
2014-01-18 15:00:23 +0100

    Avancement loading

--- src/tropicalescape/Game.java
+++ src/tropicalescape/Game.java
@@ -1,4 +1,9 @@
 package tropicalescape;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -12,9 +17,7 @@ import org.newdawn.slick.SlickException;
 
 import tropicalescape.enemies.Enemy;
 
-
 public class Game extends BasicGame {
-
 
 	private List<Enemy> enemies = new ArrayList<Enemy>();
 	private List<Flag> flags = new ArrayList<Flag>();
@@ -26,11 +29,68 @@ public class Game extends BasicGame {
 
 	@Override
 	public void init(GameContainer container) throws SlickException {
+		/*String lvlName = "res/levels/test.lvl";
+		try {
+			loadLevel(lvlName);
+		} catch (IOException e) {
+			new SlickException("Problème au chargement du niveau " + lvlName
+					+ " : " + e.getMessage());
+		}*/
 	}
 
 	@Override
 	public void render(GameContainer container, Graphics g)
 			throws SlickException {
+		for (Enemy enemy : enemies) {
+			enemy.render(g);
+		}
+		for (Flag flag : flags) {
+			flag.render(g);
+		}
+		for (Ship ship : ships) {
+			ship.render(g);
+		}
+	}
+
+	public void loadLevel(String path) throws IOException {
+		File file = new File(path);
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			String text = null;
+			while ((text = reader.readLine()) != null) {
+				String[] tokens = text.split(" ");
+				if (tokens[0].equals("ISLAND")) {
+					if (tokens.length == 3) {
+						// TODO à faire
+					}
+				} else if (tokens[0].equals("SHIP")) {
+					if (tokens.length == 3) {
+						Ship ship = new Ship(Float.parseFloat(tokens[1]),
+								Float.parseFloat(tokens[2]));
+						ships.add(ship);
+					}
+				} else if (tokens[0].equals("FLAG")) {
+					if (tokens.length == 4) {
+						Flag flag = new Flag(tokens[1],
+								Float.parseFloat(tokens[2]),
+								Float.parseFloat(tokens[3]));
+						flags.add(flag);
+					}
+				}
+			}
+			if (flags.size() != 0) {
+				for (Ship s : ships) {
+					s.setNextFlag(flags.get(0));
+				}
+			} else {
+				System.out.println("Pas de flags sur la map !");
+			}
+		} finally {
+			if (reader != null) {
+				reader.close();
+			}
+		}
 	}
 
 	@Override
@@ -55,13 +115,15 @@ public class Game extends BasicGame {
 
 			// TODO : peut être le remplacer par un rectangle de colision si
 			// ship trop rapide
-			if (flag.getX() == ship.getX() && flag.getY() == ship.getY()) {
-				int i = flags.indexOf(flag);
-				if (i == flags.size()) // Dernier flag atteint
-				{
-					ship.setNextFlag(null);
-				} else {
-					ship.setNextFlag(flags.get(i + 1));
+			if (flag != null) {
+				if (flag.getX() == ship.getX() && flag.getY() == ship.getY()) {
+					int i = flags.indexOf(flag);
+					if (i == flags.size()) // Dernier flag atteint
+					{
+						ship.setNextFlag(null);
+					} else {
+						ship.setNextFlag(flags.get(i + 1));
+					}
 				}
 			}
 		}
@@ -72,8 +134,10 @@ public class Game extends BasicGame {
 	public static void main(String[] args) {
 		try {
 			AppGameContainer appgc;
-			appgc = new AppGameContainer(new Game("Tropical Escape !"));
+			Game game = new Game("Tropical Escape !");
+			appgc = new AppGameContainer(game);
 			appgc.setDisplayMode(640, 480, false);
+
 			appgc.start();
 		} catch (SlickException ex) {
 			Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
