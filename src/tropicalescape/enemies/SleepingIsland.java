403030083492214464685d50cdd99c215cdcbfbd
Vienne <alexandre.vienne@gmail.com>
2014-01-18 18:35:39 +0100

    caca

--- src/tropicalescape/enemies/SleepingIsland.java
+++ src/tropicalescape/enemies/SleepingIsland.java
@@ -7,12 +7,12 @@ public class SleepingIsland extends Island {
 	
 	
 	
-	private HitboxAnimation up = HitboxAnimationFactory.create(new String[] {"res/animations/sleeping-island/1.png","res/animations/sleeping-island/2.png"}, new String[] {"res/hitboxes/sleeping-island/1.txt","res/hitboxes/sleeping-island/2.txt"}, 500);
-	private HitboxAnimation sleep = HitboxAnimationFactory.create(new String[] {"res/animations/sleeping-island/2.png","res/animations/sleeping-island/1.png"}, new String[] {"res/hitboxes/sleeping-island/2.txt","res/hitboxes/sleeping-island/1.txt"}, 500);
+	private HitboxAnimation up = HitboxAnimationFactory.create(new String[] {"res/animations/sleeping-island/1.png","res/animations/sleeping-island/2.png","res/animations/sleeping-island/3.png"}, new String[] {"res/hitboxes/sleeping-island/1.txt","res/hitboxes/sleeping-island/2.txt","res/hitboxes/sleeping-island/3.txt"}, 500);
+	private HitboxAnimation sleep = HitboxAnimationFactory.create(new String[] {"res/animations/sleeping-island/3.png","res/animations/sleeping-island/2.png","res/animations/sleeping-island/1.png"}, new String[] {"res/hitboxes/sleeping-island/3.txt","res/hitboxes/sleeping-island/2.txt","res/hitboxes/sleeping-island/1.txt"}, 500);
 	
 	public static final int SLEEPING = 0 ;
 	public static final int AWAKE = 1 ;
-	public static final int TIMER_DURATION = 1000; 
+	public static final int TIMER_DURATION = 5000; 
 
 	private int state =  SleepingIsland.AWAKE; 
 	private int timer =  SleepingIsland.TIMER_DURATION; 
