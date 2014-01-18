4006b45ee75ac964789a82d55ac71f12bf1bfb72
eoger <edouard.oger@gmail.com>
2014-01-18 11:53:57 +0100

    Changement signatures GameObject

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,5 +1,7 @@
-import org.newdawn.slick.Renderable;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
 
-public interface GameObject extends Renderable {
-	public void update(int delta);
+public interface GameObject {
+	public void render(GameContainer gc, Graphics g);
+	public void update(GameContainer gc, int delta);
 }
