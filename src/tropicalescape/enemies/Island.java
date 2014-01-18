d380ee348ffe1ac7f5b7f09a8ded75296736a115
Vienne <alexandre.vienne@gmail.com>
2014-01-18 16:58:54 +0100

    add function for create custom hitbox on Sleeping Island

--- src/tropicalescape/enemies/Island.java
+++ src/tropicalescape/enemies/Island.java
@@ -10,6 +10,10 @@ public abstract class Island extends Enemy {
 	public Island() {
 		super(makeHitboxAnimation());
 	}
+	
+	public Island(HitboxAnimation hb){
+		super(hb);
+	}
 
 	private static HitboxAnimation makeHitboxAnimation() {
 		// TODO Auto-generated method stub
